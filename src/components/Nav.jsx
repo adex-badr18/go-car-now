938fa8cba038de72696067b5ff976f11dc711a4d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-21 17:25:36 +0100

    Refactor navbar to use header tag

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
-        <nav>
+        <header>
             <Link to='/'>
                 <div className="logo-container">
                     <img src={logo} className='logo-img' alt="Sport car" />
                     <h2 className='logo-text'>Go<span>Car</span>Now</h2>
                 </div>
             </Link>
-
-            <ul className="nav">
-                <li><Link to='about'>About</Link></li>
-                <li><Link to='vans'>Cars</Link></li>
-            </ul>
-        </nav>
+            
+            <nav>
+                <ul className="nav">
+                    <li><Link to='about'>About</Link></li>
+                    <li><Link to='vans'>Cars</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
