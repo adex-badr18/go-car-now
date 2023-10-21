6db63790e730a190f13365adc54b2678bb4e6219
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-21 11:50:43 +0100

    Render Nav & setup Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <h1>
-            Hello world!
-        </h1>
+        <div className='container'>
+            <Nav />
+
+            <BrowserRouter>
+                <Routes>
+
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-
-        </Routes>
-    </BrowserRouter>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />,);
