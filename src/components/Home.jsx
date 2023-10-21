66f4b3b4bb62eada57adc46538eaf70fba062bde
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-21 13:52:49 +0100

    Style home section content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,12 @@ export default function Home() {
         <section className="home">
             <h1 className="hero-header">Instant Mobility at Your Fingertips. Explore the City with Ease.</h1>
 
-            <p className="hero-description">Whether it's a spontaneous road trip or daily commuting, our fleet of reliable vehicles is just a tap away.</p>
+            <p className="hero-description">
+                Experience freedom, flexibility, and total convenience on your road trip or daily commuting.
+                <span>
+                    Wherever you go, whenever you need – our fleet of reliable vehicles take you there.
+                </span>
+            </p>
 
             <button className="cta-btn">Find your car</button>
         </section>
