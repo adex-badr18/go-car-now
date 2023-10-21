86a4aef0a344887900adad71e64069c8fb4a3ac1
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-21 13:29:40 +0100

    Add content to the home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 export default function Home() {
     return (
         <section className="home">
-            <h1>Home Page</h1>
+            <h1 className="hero-header">Instant Mobility at Your Fingertips. Explore the City with Ease.</h1>
+
+            <p className="hero-description">Whether it's a spontaneous road trip or daily commuting, our fleet of reliable vehicles is just a tap away.</p>
+
+            <button className="cta-btn">Find your car</button>
         </section>
     )
 }
