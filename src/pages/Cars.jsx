4e72d7ccc77ffd56094746732d1008c74d10ba4f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 15:15:23 +0100

    Modify car list styles

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -4,25 +4,52 @@ import '../server';
 
 export default function Cars() {
     const [carsArray, setCarsArray] = useState([]);
+    const [carTypes, setCarTypes] = useState([]);
 
     useEffect(() => {
         fetch('/api/cars')
             .then(res => res.json())
-            .then(data => setCarsArray(data.cars))
+            .then(data => {
+                setCarsArray(data.cars);
+            })
     }, []);
 
+    useEffect(() => {
+        setCarTypes(() => {
+            const types = [];
+            carsArray.forEach(car => {
+                if (!types.includes(car.type)) {
+                    types.push(car.type)
+                }
+            })
+            return types;
+        });
+    }, [carsArray]);
+
     const carsElement = carsArray.map(car => (
-        <CarCard 
-            key={car.id} 
-            imgUrl={car.imageUrl} 
-            name={car.name} 
-            price={car.price} 
+        <CarCard
+            key={car.id}
+            imgUrl={car.imageUrl}
+            name={car.name}
+            price={car.price}
             type={car.type} />
-    ))
+    ));
+
+    console.log(carTypes)
+    const carTypesElement = carTypes.map(type => {
+        <div key={type} className="car-type">{type}</div>
+    });
+    console.log(carTypesElement);
 
     return (
         <section className="cars">
-            <h1>Cars page goes here 🚙</h1>
+            <h1 className="cars-header">Explore our van options</h1>
+
+            <div className="filter-container">
+                {carTypesElement}
+
+                <div className="car-type clear-filter">Clear filters</div>
+            </div>
 
             <div className="cars-list">
                 {carsElement}
