00887df720bd68dc5e80f16973cf3986de1e73a5
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 19:05:13 +0100

    Fetch details of the clicked car with useParams

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import '../server';
+
 export default function CarDetail() {
+    const params = useParams();
+    const [car, setCar] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/cars/${params.id}`)
+            .then(res => res.json())
+            .then(data => setCar(data.cars))
+
+    }, [params.id])
+
     return (
         <section className="car-detail">
             <h1>Car detail goes here.</h1>
