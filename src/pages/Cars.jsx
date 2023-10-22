e44d1446f9a0a0174d7050bb8f73244032dd3678
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 12:20:49 +0100

    Render list of cars

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -11,9 +11,22 @@ export default function Cars() {
             .then(data => setCarsArray(data.cars))
     }, []);
 
+    const carsElement = carsArray.map(car => (
+        <CarCard 
+            key={car.id} 
+            imgUrl={car.imageUrl} 
+            name={car.name} 
+            price={car.price} 
+            type={car.type} />
+    ))
+
     return (
         <section className="cars">
             <h1>Cars page goes here 🚙</h1>
+
+            <div className="cars-list">
+                {carsElement}
+            </div>
         </section>
     )
 }
