a5f1db9bb940220ef082e6ca718f573de9770ec1
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 16:52:56 +0100

    Style hover & selected states of filter elements

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -37,7 +37,7 @@ export default function Cars() {
 
     console.log(carTypes)
     const carTypesElement = carTypes.map(type => {
-        return <div key={type} className="car-type">{type}</div>
+        return <div key={type} className={`car-type ${type}`}>{type}</div>
     });
     console.log(carTypesElement);
 
