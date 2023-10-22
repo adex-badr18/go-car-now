f7b52329e22ba6e7e722fb3f2fd753aad65ed9d9
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 11:48:03 +0100

    Customize classname for car-type element

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,14 +1,21 @@
-export default function CarCard({imgUrl, name, price, type}) {
+export default function CarCard({ imgUrl, name, price, type }) {
+    const typeClass = 'car-type ';
+    typeClass += type === 'luxury' ? 'luxury' : '';
+    typeClass += type === 'simple' ? 'simple' : '';
+    typeClass += type === 'rugged' ? 'rugged' : '';
+
     return (
         <div className="car-card">
-            <img src={imgUrl} alt={name} className="car-img" />
+            <div className="card-img-container">
+                <img src={imgUrl} alt={name} className="car-img" />
+            </div>
 
             <div className="car-info">
                 <h3 className="car-name">{name}</h3>
                 <h3 className="car-price">${price} <span>/day</span></h3>
             </div>
 
-            <div className="car-type">{type}</div>
+            <div className={typeClass}>{type}</div>
         </div>
     )
 }
