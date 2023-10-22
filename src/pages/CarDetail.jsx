0136f264226a256bd0d142ea71010f2e5e117fdf
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-22 20:01:33 +0100

    Update CardDetail markup

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 import '../server';
 
@@ -17,22 +18,22 @@ export default function CarDetail() {
 
     return (
         <section className="car-detail">
-            <div className="back-container">
-                <BiArrowBack className="back--icon" />
+            <Link to='/cars' className='back-link'>
+                <BiArrowBack className="back-icon" />
                 <span className="back-text">Back to all cars</span>
-            </div>
+            </Link>
 
-            <img src="" alt="" className="car-img" />
+            <img src={car.imageUrl} alt={car.name} className="car-img" />
 
-            <div className="car-type"></div>
+            <div className={`car-type ${car.type}`}>{car.type}</div>
 
-            <h2 className="name"></h2>
+            <h2 className="name">{car.name}</h2>
 
-            <h3 className="price">$60<span>/day</span></h3>
+            <h3 className="price">${car.price}<span>/day</span></h3>
 
-            <p className="car-description"></p>
+            <p className="car-description">{car.description}</p>
 
-            <Link to=''>Rent this van</Link>
+            <Link to='' className='link-btn car-link'>Rent this van</Link>
         </section>
     )
 }
