f85aba803c7b7feeb6b7ca36115eba5c1333583a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-23 14:04:00 +0100

    Style listed cars panel

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -43,39 +43,39 @@ export default function Dashboard() {
                 </div>
 
                 <div className="dash--cars-list">
-                    <div className="dash--car">
+                    <div className="dash--car-wrapper">
                         <img className="dash--car-thumb" src="/pickup-van.webp" alt="" />
 
                         <div className="dash--car">
                             <div className="dash--car-info">
                                 <h4 className="dash--car-name">Pickup Van</h4>
-                                <h5 className="dash--car-price">$100</h5>
+                                <h5 className="dash--car-price">$100/day</h5>
                             </div>
 
                             <h5 className="dash-link">Edit</h5>
                         </div>
                     </div>
 
-                    <div className="dash--car">
+                    <div className="dash--car-wrapper">
                         <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
 
                         <div className="dash--car">
                             <div className="dash--car-info">
                                 <h4 className="dash--car-name">Hatchback Golf</h4>
-                                <h5 className="dash--car-price">$120</h5>
+                                <h5 className="dash--car-price">$120/day</h5>
                             </div>
 
                             <h5 className="dash-link">Edit</h5>
                         </div>
                     </div>
 
-                    <div className="dash--car">
+                    <div className="dash--car-wrapper">
                         <img className="dash--car-thumb" src="/mini-truck.webp" alt="" />
 
                         <div className="dash--car">
                             <div className="dash--car-info">
                                 <h4 className="dash--car-name">Mini Truck</h4>
-                                <h5 className="dash--car-price">$80</h5>
+                                <h5 className="dash--car-price">$80/day</h5>
                             </div>
 
                             <h5 className="dash-link">Edit</h5>
