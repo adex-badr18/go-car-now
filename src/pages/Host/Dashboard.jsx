673a20dfc139477f374a064210dc25f3828f0e78
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-23 11:59:55 +0100

    Create & style overview panel of dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -4,13 +4,24 @@ import { Link } from "react-router-dom";
 export default function Dashboard() {
     return (
         <section className="host-dashboard">
-            <div className="host-nav">
+            <div className="dash-nav">
                 <Nav>
                     <li><Link to='/host'>Dashboard</Link></li>
                     <li><Link to='/host/income'>Income</Link></li>
                     <li><Link to='/host/cars'>Cars</Link></li>
                     <li><Link to='/host/reviews'>ٌReviews</Link></li>
                 </Nav>
+            </div>
+
+            <div className="dash-overview">
+                <h2>Welcome!</h2>
+
+                <div className="income-control">
+                    <h5>Income last <span className="bold-text">30 days</span></h5>
+                    <h5 className="bold-text">Details</h5>
+                </div>
+
+                <h1 className="income">$2,260</h1>
             </div>
 
         </section>
