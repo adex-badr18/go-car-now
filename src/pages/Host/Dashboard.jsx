f057077acdfe38292ecfe95d1d2701af35d5ccda
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-23 13:25:10 +0100

    Add cars list markup on dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,6 +1,6 @@
 import Nav from "../../components/Nav";
 import { Link } from "react-router-dom";
-import {AiFillStar} from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 
 export default function Dashboard() {
     return (
@@ -19,7 +19,7 @@ export default function Dashboard() {
 
                 <div className="income-control">
                     <h5>Income last <span className="bold-text">30 days</span></h5>
-                    <h5 className="detail-link">Details</h5>
+                    <h5 className="dash-link">Details</h5>
                 </div>
 
                 <h1 className="income">$2,260</h1>
@@ -32,7 +32,56 @@ export default function Dashboard() {
                     5.0<span>/5</span>
                 </h5>
 
-                <h5 className="detail-link">Details</h5>
+                <h5 className="dash-link">Details</h5>
+            </div>
+
+            <div className="listed-cars">
+                <div className="listed-cars-header">
+                    <h4 className="listed-cars-title">Your listed cars</h4>
+
+                    <h5 className="dash-link">View all</h5>
+                </div>
+
+                <div className="dash--cars-list">
+                    <div className="dash--car">
+                        <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
+
+                        <div className="dash--car">
+                            <div className="dash--car-info">
+                                <h4 className="dash--car-name">Modest Explorer</h4>
+                                <h5 className="dash--car-price"></h5>
+                            </div>
+
+                            <h5 className="dash-link">Edit</h5>
+                        </div>
+                    </div>
+                    
+                    <div className="dash--car">
+                        <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
+
+                        <div className="dash--car">
+                            <div className="dash--car-info">
+                                <h4 className="dash--car-name">Modest Explorer</h4>
+                                <h5 className="dash--car-price"></h5>
+                            </div>
+
+                            <h5 className="dash-link">Edit</h5>
+                        </div>
+                    </div>
+
+                    <div className="dash--car">
+                        <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
+
+                        <div className="dash--car">
+                            <div className="dash--car-info">
+                                <h4 className="dash--car-name">Modest Explorer</h4>
+                                <h5 className="dash--car-price"></h5>
+                            </div>
+
+                            <h5 className="dash-link">Edit</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </section>
