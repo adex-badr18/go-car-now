cd0d3085d061311e16ade35e7a9c7f04213a926d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-23 13:30:39 +0100

    Add listed cars markup to dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -35,8 +35,8 @@ export default function Dashboard() {
                 <h5 className="dash-link">Details</h5>
             </div>
 
-            <div className="listed-cars">
-                <div className="listed-cars-header">
+            <div className="dash--listed-cars">
+                <div className="dash--listed-cars-header">
                     <h4 className="listed-cars-title">Your listed cars</h4>
 
                     <h5 className="dash-link">View all</h5>
@@ -44,25 +44,12 @@ export default function Dashboard() {
 
                 <div className="dash--cars-list">
                     <div className="dash--car">
-                        <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
+                        <img className="dash--car-thumb" src="/pickup-van.webp" alt="" />
 
                         <div className="dash--car">
                             <div className="dash--car-info">
-                                <h4 className="dash--car-name">Modest Explorer</h4>
-                                <h5 className="dash--car-price"></h5>
-                            </div>
-
-                            <h5 className="dash-link">Edit</h5>
-                        </div>
-                    </div>
-                    
-                    <div className="dash--car">
-                        <img className="dash--car-thumb" src="/hatchback.jpg" alt="" />
-
-                        <div className="dash--car">
-                            <div className="dash--car-info">
-                                <h4 className="dash--car-name">Modest Explorer</h4>
-                                <h5 className="dash--car-price"></h5>
+                                <h4 className="dash--car-name">Pickup Van</h4>
+                                <h5 className="dash--car-price">$100</h5>
                             </div>
 
                             <h5 className="dash-link">Edit</h5>
@@ -74,8 +61,21 @@ export default function Dashboard() {
 
                         <div className="dash--car">
                             <div className="dash--car-info">
-                                <h4 className="dash--car-name">Modest Explorer</h4>
-                                <h5 className="dash--car-price"></h5>
+                                <h4 className="dash--car-name">Hatchback Golf</h4>
+                                <h5 className="dash--car-price">$120</h5>
+                            </div>
+
+                            <h5 className="dash-link">Edit</h5>
+                        </div>
+                    </div>
+
+                    <div className="dash--car">
+                        <img className="dash--car-thumb" src="/mini-truck.webp" alt="" />
+
+                        <div className="dash--car">
+                            <div className="dash--car-info">
+                                <h4 className="dash--car-name">Mini Truck</h4>
+                                <h5 className="dash--car-price">$80</h5>
                             </div>
 
                             <h5 className="dash-link">Edit</h5>
