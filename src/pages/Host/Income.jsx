9e41ff0390e4a919c2f919617a0043d79cbb59b6
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-23 14:11:39 +0100

    Scaffold Income & Reviews pages

---
+++ src/pages/Host/Income.jsx
@@ -0,0 +1,7 @@
+export default function Income() {
+    return (
+        <section className="income">
+            <h1>Income content goes here</h1>
+        </section>
+    )
+}
---
+++ src/pages/Host/Reviews.jsx
@@ -0,0 +1,7 @@
+export default function Reviews() {
+    return (
+        <section className="reviews">
+            <h1>Reviews content goes here</h1>
+        </section>
+    )
+}
