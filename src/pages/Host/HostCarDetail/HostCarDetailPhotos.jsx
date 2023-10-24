b28b6d740718518a780e559847f696414c23198b
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-24 20:48:44 +0100

    Create HostCarDetailPhotos page

---
+++ src/pages/Host/HostCarDetail/HostCarDetailPhotos.jsx
@@ -0,0 +1,7 @@
+export default function HostCarDetailPhotos() {
+    return (
+        <div className="host-car-detail-photos">
+            <img src="/mini-truck.webp" alt="Mini truck" />
+        </div>
+    )
+}
