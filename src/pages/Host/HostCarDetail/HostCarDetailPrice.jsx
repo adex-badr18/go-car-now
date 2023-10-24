41e350f93542b918cfc8fc4bc49276b0b4ba73dc
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-24 20:48:19 +0100

    Create HostCarDetailPrice page

---
+++ src/pages/Host/HostCarDetail/HostCarDetailPrice.jsx
@@ -0,0 +1,7 @@
+export default function HostCarDetailPrice() {
+    return (
+        <div className="host-car-detail-price">
+            <h3 className="car-detail-price">$60<span>/day</span></h3>
+        </div>
+    )
+}
