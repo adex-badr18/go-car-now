db6be12a2894b3906fdd8abb260a2e24a57fbbf4
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-24 21:33:51 +0100

    Update Review page markup

--- src/pages/Host/Reviews.jsx
+++ src/pages/Host/Reviews.jsx
@@ -1,7 +1,46 @@
+import { AiFillStar } from "react-icons/ai";
+import Nav from "../../components/Nav";
+
 export default function Reviews() {
     return (
         <section className="reviews">
-            <h1>Reviews content goes here</h1>
+
+            <h2>
+                Your reviews
+                <span>last <span>30 days</span></span>
+            </h2>
+
+            <div className="review-chart">
+                <img src="" alt="" />
+            </div>
+
+            <div className="review-wrapper">
+                <div className="rating-stars">
+                    <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar />
+                </div>
+
+                <div className="review">
+                    <h5 className="review-header">Elliot <span>December 1, 2022</span></h5>
+
+                    <p className="review-body">
+                        The beach bum is such as awesome van! Such as comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!
+                    </p>
+                </div>
+            </div>
+
+            <div className="review-wrapper">
+                <div className="rating-stars">
+                    <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar />
+                </div>
+
+                <div className="review">
+                    <h5 className="review-header">Sandy <span>November, 2022</span></h5>
+
+                    <p className="review-body">
+                        This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
