c278fe5d8efb55f617d16b0bebff13f052fa7162
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-24 17:58:24 +0100

    Style host car details tab

--- src/pages/Host/HostCarDetail.jsx
+++ src/pages/Host/HostCarDetail.jsx
@@ -42,10 +42,15 @@ export default function HostCarDetail() {
                     </li>
                 </Nav>
 
-                <p className="">Name: <span>Mini Truck</span></p>
-                <p className="">Category: <span>Simple</span></p>
-                <p className="car-description">Mini trucks are popular for commercial purposes in crowded cities where larger vehicles might face challenges in maneuverability and parking. They are often used for small-scale deliveries, especially in narrow streets and congested areas. Due to their size, they are economical and practical for businesses dealing with light cargo loads.</p>
-                <p className="">Visibility: <span>Public</span></p>
+                <div className="host-car-detail-description">
+                    <p className="">Name: <span>Mini Truck</span></p>
+
+                    <p className="">Category: <span>Simple</span></p>
+
+                    <p className="car-description">Description: <span>Mini trucks are popular for commercial purposes in crowded cities where larger vehicles might face challenges in maneuverability and parking. They are often used for small-scale deliveries, especially in narrow streets and congested areas. Due to their size, they are economical and practical for businesses dealing with light cargo loads.</span></p>
+
+                    <p className="">Visibility: <span>Public</span></p>
+                </div>
             </div>
         </section>
     )
