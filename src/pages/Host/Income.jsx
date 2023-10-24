7d7917d081305a73806e6b03231ea061b4484d47
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-24 22:40:42 +0100

    Create Host income markup

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -1,7 +1,39 @@
 export default function Income() {
     return (
         <section className="income">
-            <h1>Income content goes here</h1>
+            <h2>Income</h2>
+
+            <h5 className="">Last <span>30 days</span></h5>
+
+            <h1 className="total-income">$2,260</h1>
+
+            <div className="income-chart">
+                <img src="/income-chart.png" alt="Income Chart" />
+            </div>
+
+            <div className="transactions-container">
+                <h4 className="transactions-header">
+                    Your transactions (3)
+                    <span>Last <span>30 days</span></span>
+                </h4>
+
+                <div className="transactions-list">
+                    <div className="transaction">
+                        <h3 className="amount">$720</h3>
+                        <h5 className="date">1/12/22</h5>
+                    </div>
+
+                    <div className="transaction">
+                        <h3 className="amount">$560</h3>
+                        <h5 className="date">10/11/22</h5>
+                    </div>
+                    
+                    <div className="transaction">
+                        <h3 className="amount">$980</h3>
+                        <h5 className="date">23/11/22</h5>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
