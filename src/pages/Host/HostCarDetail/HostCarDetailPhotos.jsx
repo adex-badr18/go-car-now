6b5ad297d32ac034c1ca81f38e6c4bbcef4cfd59
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-25 11:53:33 +0100

    Fetch & render photos in HostCarDetailPhotos

--- src/pages/Host/HostCarDetail/HostCarDetailPhotos.jsx
+++ src/pages/Host/HostCarDetail/HostCarDetailPhotos.jsx
@@ -1,7 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function HostCarDetailPhotos() {
+    const [car, setCar] = useState(null);
+    const carId = useParams().id;
+
+    useEffect(() => {
+        fetch(`/api/host/cars/${carId}`)
+            .then(res => res.json())
+            .then(data => setCar(data.cars[0]));
+    }, []);
+
     return (
         <div className="host-car-detail-photos">
-            <img src="/mini-truck.webp" alt="Mini truck" />
+            <img src={car?.imageUrl} alt={car?.name} />
         </div>
     )
 }
