d9ce14bc1faa5ae1900617b17d405d83ed702974
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-25 11:55:35 +0100

    Fetch & render car price in HostCarDetailPrice

--- src/pages/Host/HostCarDetail/HostCarDetailPrice.jsx
+++ src/pages/Host/HostCarDetail/HostCarDetailPrice.jsx
@@ -1,7 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function HostCarDetailPrice() {
+    const [car, setCar] = useState(null);
+    const carId = useParams().id;
+
+    useEffect(() => {
+        fetch(`/api/host/cars/${carId}`)
+            .then(res => res.json())
+            .then(data => setCar(data.cars[0]));
+    }, []);
+
     return (
         <div className="host-car-detail-price">
-            <h3 className="car-detail-price">$60<span>/day</span></h3>
+            <h3 className="car-detail-price">${car?.price}<span>/day</span></h3>
         </div>
     )
 }
