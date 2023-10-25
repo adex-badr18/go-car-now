55ceac2906f3fc95d00e6be19289bd64225dcef9
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-25 11:41:06 +0100

    Fetch & render host car details

--- src/pages/Host/HostCarDetail/HostCarDetail.jsx
+++ src/pages/Host/HostCarDetail/HostCarDetail.jsx
@@ -1,11 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function HostCarDetail() {
+    const [car, setCar] = useState(null);
+    const carId = useParams().id;
+
+    useEffect(() => {
+        fetch(`/api/host/cars/${carId}`)
+            .then(res => res.json())
+            .then(data => setCar(data.cars[0]));
+    }, []);
+
     return (
         <div className="host-car-detail-description">
-            <p className="">Name: <span>Mini Truck</span></p>
+            <p className="">Name: <span>{car?.name}</span></p>
 
-            <p className="">Category: <span>Simple</span></p>
+            <p className="">Category: <span>{car?.type}</span></p>
 
-            <p className="car-description">Description: <span>Mini trucks are popular for commercial purposes in crowded cities where larger vehicles might face challenges in maneuverability and parking. They are often used for small-scale deliveries, especially in narrow streets and congested areas. Due to their size, they are economical and practical for businesses dealing with light cargo loads.</span></p>
+            <p className="car-description">Description: <span>{car?.description}</span></p>
 
             <p className="">Visibility: <span>Public</span></p>
         </div>
