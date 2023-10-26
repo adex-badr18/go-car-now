54357567838bac6971f00f36e89c433fac47df74
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-26 17:41:59 +0100

    Create 404 page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function PageNotFound() {
+    return (
+        <section className="page-not-found">
+            <h1>Sorry, the page you were looking for does not exist.</h1>
+            <Link to='/'>Return to home</Link>
+        </section>
+    )
+}
