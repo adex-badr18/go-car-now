37f4ffe050501b7216ea4d8ef4ddb1df49a50fbd
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-27 19:18:35 +0100

    Add a user profile icon to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '/car-logo.png';
 import { Link, NavLink } from 'react-router-dom';
 import Nav from './Nav';
+import { BsPersonCircle } from 'react-icons/bs';
 
 export default function Header() {
     const activeStyle = {
@@ -23,6 +24,7 @@ export default function Header() {
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='host'>Host</NavLink></li>
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='cars'>Cars</NavLink></li>
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='about'>About</NavLink></li>
+                <li><Link to='hostlogin'><BsPersonCircle /></Link></li>
             </Nav>
         </header>
     )
