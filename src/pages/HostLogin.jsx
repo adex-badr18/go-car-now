69cca1296a4c93c0f1a15e5adc147c5301bb4f25
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-27 17:54:37 +0100

    Store the form value in state on input change

--- src/pages/HostLogin.jsx
+++ src/pages/HostLogin.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function HostLogin() {
+    const [loginFormData, setLoginFormData] = useState({email: '', password: ''});
+
+    function updateFormData(e) {
+        const {name, value} = e.target;
+
+        setLoginFormData(prevData => {
+            return {
+                ...prevData,
+                [name]: value
+            }
+        });
+    }
+
+    console.log(loginFormData);
+
     return (
         <section className="host-login">
             <h1 className="host-login-header">Sign in to your account</h1>
 
-            <form action="" className="host-login-form">
-                <input type="email" name="email" id="email" placeholder="Email address" aria-label="Email address" />
+            <form className="host-login-form">
+                <input
+                    type="email"
+                    value={loginFormData.email}
+                    onChange={(e) => updateFormData(e)}
+                    name="email"
+                    placeholder="Email address"
+                    aria-label="Email address" />
 
-                <input type="password" name="password" placeholder="Password" id="password" aria-label="Password" />
+                <input
+                    type="password"
+                    value={loginFormData.password}
+                    onChange={(e) => updateFormData(e)}
+                    name="password"
+                    placeholder="Password"
+                    aria-label="Password" />
 
                 <button className="sign-in-btn">Sign in</button>
             </form>
