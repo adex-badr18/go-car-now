900023f5241404f59b79d67f2360a51d91e04aab
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-27 20:56:26 +0100

    Refactor profile-avatar style on hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='host'>Host</NavLink></li>
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='cars'>Cars</NavLink></li>
                 <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='about'>About</NavLink></li>
-                <li><Link to='hostlogin'><BsPersonCircle /></Link></li>
+                <li><Link to='hostlogin' className='profile-avatar'><BsPersonCircle /></Link></li>
             </Nav>
         </header>
     )
