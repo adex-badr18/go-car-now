6d343e0bf0e775334e0d4cb2426724e454c59e0d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-28 23:10:07 +0100

    Add logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import logo from '/car-logo.png';
 import { Link, NavLink } from 'react-router-dom';
 import Nav from './Nav';
 import { BsPersonCircle } from 'react-icons/bs';
+import { BiLogIn } from 'react-icons/bi';
+
 
 export default function Header() {
     const activeStyle = {
@@ -9,7 +11,8 @@ export default function Header() {
         color: "#161616",
         paddingBottom: "1px",
         borderBottom: "3px solid #161616"
-    }
+    };
+    const authenticated = localStorage.getItem('authenticated') || false;
 
     return (
         <header>
@@ -21,10 +24,18 @@ export default function Header() {
             </Link>
 
             <Nav>
-                <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='host'>Host</NavLink></li>
-                <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='cars'>Cars</NavLink></li>
-                <li><NavLink style={({isActive}) => isActive ? activeStyle : null} to='about'>About</NavLink></li>
-                <li><Link to='hostlogin' className='profile-avatar'><BsPersonCircle /></Link></li>
+                <li><NavLink style={({ isActive }) => isActive ? activeStyle : null} to='host'>Host</NavLink></li>
+                <li><NavLink style={({ isActive }) => isActive ? activeStyle : null} to='cars'>Cars</NavLink></li>
+                <li><NavLink style={({ isActive }) => isActive ? activeStyle : null} to='about'>About</NavLink></li>
+                <li>
+                    {
+                        authenticated ?
+                            <Link to='hostlogin' onClick={() => localStorage.removeItem('authenticated')} className='profile-avatar'><BsPersonCircle /></Link> :
+                            <Link to='hostlogin' className='profile-avatar'>
+                                <BiLogIn />
+                            </Link>
+                    }
+                </li>
             </Nav>
         </header>
     )
