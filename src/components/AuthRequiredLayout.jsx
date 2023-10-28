d13b3bf03a27db6f9aad5dc0276997a27bbb9437
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-28 14:26:38 +0100

    Create & code the AuthRequiredLayout logic

---
+++ src/components/AuthRequiredLayout.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function AuthRequiredLayout() {
+    const authenticated = true;
+
+    if (authenticated) {
+        return <Outlet/>
+    }
+
+    return <Navigate to='hostlogin' />
+}
