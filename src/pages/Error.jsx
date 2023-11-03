35b0162046213b7c486dad4f490b7020dab260b2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-03 17:26:27 +0100

    Create Error page component

---
+++ src/pages/Error.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <section className="error-container">
+            <h1>Something went wrong :(</h1>
+            <p>Please try again or refresh your page.</p>
+        </section>
+    )
+}
